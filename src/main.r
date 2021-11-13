5a74235a24c810456083847543925222b3972731
Emmanuel Olowe <eo32@st-andrews.ac.uk>
2021-11-13 01:53:03 +0000

    added

--- src/main.r
+++ src/main.r
@@ -7,3 +7,6 @@ parameter <- "palmitoleic"
 north <- olive[olive$region == "Northern Italy", c("region", parameter)]
 south <- olive[olive$region == "Southern Italy", c("region", parameter)]
 sardinia <- olive[olive$region == "Sardinia", c("region", parameter)]
+
+# Makes sure the data
+res <- aov(palmitoleic ~ region, data = olive)
